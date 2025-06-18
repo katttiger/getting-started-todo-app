d0bbef9edb72c98cbfc012742fd5aca539d4c6ba
katttiger <katttiger@gmail.com>
2025-06-18 11:03:27 +0200

    Initial Commit

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,7 @@
-const GREETING = 'Hello world!';
+const GREETING = ['Welcome!', 'Nice day', 'Good evening'];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETING[Math.floor(Math.random() * GREETING.length)],
     });
 };
